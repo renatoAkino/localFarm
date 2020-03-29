30dd1fc7761d9d61666804de8532a9422367cced
renatoAkino <renato.aquino@etec.sp.gov.br>
2020-03-29 05:20:40 -0300

    Layout tela de Login

--- app_usuario/local_farm/lib/main.dart
+++ app_usuario/local_farm/lib/main.dart
@@ -3,6 +3,13 @@ import 'package:localfarm/Screens/login_screen.dart';
 
 void main() {
   return runApp(MaterialApp(
+    title: "Local Farm",
     home: LoginScreen(),
+    theme: ThemeData(
+      primaryColor: Color.fromARGB(255, 51,196,155),
+      accentColor: Color.fromARGB(255, 41,158,125)
+    ),
+    debugShowCheckedModeBanner: false,
   ));
 }
+
