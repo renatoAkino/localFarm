4d0f0bbf8d9f299b784e14cd9574cad8f2fb6b9e
Gabriel Betiol Lima <gbetiol@ibm.com>
2021-01-15 00:18:23 -0300

    criacao da classe de req

--- local_farm/producer_farm/lib/Screens/deliveries/delivery_screen.dart
+++ local_farm/producer_farm/lib/Screens/deliveries/delivery_screen.dart
@@ -1,4 +1,9 @@
+
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:producerfarm/Datas/vroom_req.dart';
 
 class DeliveryScreen extends StatelessWidget {
   @override
@@ -14,8 +19,31 @@ class DeliveryScreen extends StatelessWidget {
     ),
     body: Row(
       children: [
-        Container()
+        Text(_showJSON())
       ],
-    ),);
+    ));
   }
+}
+
+_showJSON() {
+  var json = '{ "vehicles": [{"id": 1,"start": [2.35044,48.71764],"end": [2.35044,48.71764],"capacity": [30], "time_window": [1600416000,1600430400]}],"jobs": [{"id": 1,"service": 300,"location": [1.98935,48.701]},{"id": 2,"service": 300,"location": [2.03655,48.61128]},{"id": 5,"service": 300,"location": [2.28325,48.5958]},{"id": 6,"service": 300,"location": [2.89357,48.90736]}]}';
+  //Para montar a req baseada no banco vai precisar:
+  // loc da farm (em coordernadas) - (select * from farm where email/id =
+  // loc dos clientes (em coordenadas) - ()
+  var vehicleId = 1;
+  var start = [2.35044, 48.71764];
+  var end = [2.35044, 48.71764];
+  var capacity = [30];
+  var time_window = [1600416000, 1600430400];
+  Vehicles vehicles = Vehicles(id: vehicleId, start: start, end: end, capacity: capacity, timeWindow: time_window );
+  List<Vehicles> listVehicles = [vehicles];
+  Jobs job1 = Jobs(id: 1, service: 300, location: [1.98935, 48.701]);
+  Jobs job2 = Jobs(id: 2, service: 300, location: [2.03655, 48.61128]);
+  Jobs job3 = Jobs(id: 5, service: 300, location: [2.28325, 48.5958]);
+  Jobs job4 = Jobs(id: 6, service: 300, location: [2.89357, 48.90736]);
+  List<Jobs> listJobs = [job1, job2, job3, job4];
+  VROOMReq vroomReq = VROOMReq(vehicles: listVehicles, jobs: listJobs);
+  String req = jsonEncode(vroomReq);
+  print(req);
+  return "";
 }
